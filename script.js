cc5ca40d6b67dd2da44ee931027d1b65e8f88a49
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-07-07 09:13:21 -0400

    restructured order of how the page loads

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
+
+$("button").click(e => {
 //1. Adding a new item to the list:
 
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
 
-if (inputValue === '') {
-  alert("You must write something!");
-} else {
-  $('#list').append(li);
-}
-//2. Crossing an item out:
-function crossOut() {
-      li.toggleClass("strike");
-  }
+    let inputValue = $('#input').val();
+    if (inputValue === '') {
+    alert("You must write something!");
+    } else {
+      $('#input').val('');
+  
+  
+        let li = $('<li></li>');  
+        li.text(inputValue);
+        $('#list').append(li);
+        li.on("dblclick", function() {  
+            li.toggleClass("strike");
+        });
 
-  li.on("dblclick", function crossOut() {
-      li.toggleClass("strike");
-  });
 //3. Adding a delete button
-let crossOutButton = $('<crossOutButton></crossOutButton>');
-crossOutButton.append(document.createTextNode('X'));
-li.append(crossOutButton);
-
-//   crossOutButton.on("click", deleteListItem);
-//   function deleteListItem(){
-// 		li.addClass("delete")
-// 	}
- $('#list').sortable();
+        let crossOutButton = $('<crossOutButton></crossOutButton>');
+        crossOutButton.append(document.createTextNode('X'));
+        li.append(crossOutButton);
+        crossOutButton.on("click", function(e) {
+        let liForDeletion = e.target.parentNode;
+        $(liForDeletion).remove();
+        });
+        $('#list').sortable();
+    }
+})
 
 
 
